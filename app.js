e57ba5b8792c5d7ff3b76c814eff1f9c7a54a3fd
MostafaWaseemMahmoud <mostafawaseem22@gmail.com>
2024-04-28 00:09:42 +0300

    Make Real BackEnd Application

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const expresss = require("express");
 const app = expresss();
-
+app.use(expresss.json());
+let users = [];
 //  * HTTP Methods
 
 // Get
@@ -9,14 +10,41 @@ app.get("/", (req, res) => {
   res.send("Welcome At Home???");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("Hello World");
+app.get("/users", (req, res) => {
+  if (users.length == 0) {
+    res.status(404).send("No Users Found");
+    return;
+  } else {
+    res.status(200).send(users);
+  }
 });
 
 // Post
-// Put
-//Delete
 
+app.post("/users", (request, response) => {
+  const user = request.body;
+  const findUser = users.find((x) => x.id === request.body.id);
+
+  if (findUser) {
+    response.status(400).send("User Is Already Exist");
+    return;
+  }
+
+  users.push(user);
+  response.status(201).send("Created!");
+});
+
+//Delete
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const finduserIndex = users.findIndex((x) => x.id === id);
+  if (finduserIndex == -1) {
+    response.status(404).send("User Is Not Find");
+    return;
+  }
+  users.splice(finduserIndex, 1);
+  res.status(200).send("User Deleted");
+});
 // We Now Create A New Host Server
 
 app.listen(3000, () => {
